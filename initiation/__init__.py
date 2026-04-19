c2834154d54ee0d14800203b6a9d489561e4ae4f
Dragonfirewolf2 <alexiusmaria@gmail.com>
2026-04-19 19:47:07 +0700

    Replace Initiation cog with MoonlightRitual cog

--- initiation/__init__.py
+++ initiation/__init__.py
@@ -1,5 +1,5 @@
-from .initiation import Initiation
+from .moonlight_ritual import MoonlightRitual
 
 
 async def setup(bot):
-    await bot.add_cog(Initiation(bot))
+    await bot.add_cog(MoonlightRitual(bot))
